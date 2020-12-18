1a6c0894b7993d6648a515a6bfd7fa6336401ab3
MaximeArn <maxime-arnould@outlook.fr>
2020-12-18 15:05:25 +0100

    get all lists method

--- server/src/controllers/lists.ts
+++ server/src/controllers/lists.ts
@@ -2,17 +2,23 @@ import { Request, Response } from "express";
 import List from "../models/list";
 
 module.exports = {
-  getAllLists: (req: Request, res: Response) => {
-    res.send("get all cards");
+  getAllLists: async (req: Request, res: Response) => {
+    try {
+      const lists = await List.find();
+      res.send(lists).status(200);
+    } catch (error) {
+      console.error(error);
+      res.send(error);
+    }
   },
   createList: async ({ body: { name, color } }: Request, res: Response) => {
     try {
       const newList = new List({ name: name, color: color });
       const insertedList = await newList.save();
-      console.log(insertedList);
-      res.send(insertedList);
+
+      res.send(insertedList).status(200);
     } catch (error) {
-      console.log(error);
+      console.error(error);
       res.send(error);
     }
   },
