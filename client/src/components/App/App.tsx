0881cfbc5238fd2572751e5b50c298de41a251bf
MaximeArn <maxime-arnould@outlook.fr>
2020-12-20 00:08:01 +0100

    clean client folde init by CRA redux/typescriptr

---
+++ client/src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import "./App.scss";
+
+function App() {
+  useEffect(() => {}, []);
+  return (
+    <div className="App">
+      <h2>Hello</h2>
+    </div>
+  );
+}
+
+export default App;
