df6886c807da4fea418347cd99a13d13700a9d6a
MaximeArn <maxime-arnould@outlook.fr>
2020-12-19 18:17:28 +0100

    tweaking of the list schema

--- src/schemas/list.ts
+++ src/schemas/list.ts
@@ -1,7 +1,8 @@
 import { Schema } from "mongoose";
+import taskSchema from "./task";
 
 export default new Schema({
   name: String,
   color: { type: String, default: "#ffffff" },
-  tasks: [{ title: String, infos: String, color: String }],
+  tasks: [taskSchema],
 });
