820199f8f6acae74ada4e72be71d59a73400f794
MaximeArn <maxime-arnould@outlook.fr>
2020-12-20 16:42:55 +0100

    call to api works

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,8 +1,15 @@
 import React, { useEffect } from "react";
+import axios from "axios";
+import apiUrl from "../../environments/api";
 import "./App.scss";
 
 function App() {
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const lists = axios
+      .get(`${apiUrl}/lists`)
+      .then(({ data }) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div className="App">
       <h2>Hello</h2>
