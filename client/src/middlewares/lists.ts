debdbbec9fdfa7da4b0128a1ad51b508cd05a1bf
MaximeArn <maxime-arnould@outlook.fr>
2021-01-07 22:03:08 +0100

    create new list on db in list/create

--- client/src/middlewares/lists.ts
+++ client/src/middlewares/lists.ts
@@ -5,17 +5,27 @@ import apiUrl from "../environments/api";
 const listsMiddleware: Middleware = ({ getState, dispatch }) => (next) => (
   action
 ) => {
-  const { type } = action;
+  const { type, payload } = action;
 
   switch (type) {
     case "list/getAll":
       axios
         .get(`${apiUrl}/lists`)
         .then(({ data }) => {
-          return next({ type, payload: data });
+          next({ type, payload: data });
         })
         .catch((err) => console.log(err));
       break;
+
+    case "list/create":
+      axios
+        .post(`${apiUrl}/lists`, payload)
+        .then(({ data }) => {
+          console.log(data);
+        })
+        .catch((err) => console.log(err));
+      break;
+
     default:
       next(action);
       break;
