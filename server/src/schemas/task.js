7dbdce25f621f88a645eec429794cc4f26c3e919
MaximeArn <maxime-arnould@outlook.fr>
2021-01-08 15:19:31 +0100

    remove duplicated js files

--- server/src/schemas/task.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-exports.default = new mongoose_1.Schema({
-    title: String,
-    info: { type: String, default: null },
-    color: { type: String, default: "#fff" },
-    done: { type: Boolean, default: false },
-});
