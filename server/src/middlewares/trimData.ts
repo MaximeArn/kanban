fd96abadac0a7367fe3eaedaa97f874a54da2e75
MaximeArn <maxime-arnould@outlook.fr>
2021-02-19 17:02:15 +0100

    test if required fields are all provided

--- server/src/middlewares/trimData.ts
+++ server/src/middlewares/trimData.ts
@@ -3,11 +3,13 @@ import { Request, Response, NextFunction } from "express";
 export default ({ method, body, path }: Request, res: Response, next: NextFunction) => {
   if (method !== "GET" && method !== "DELETE") {
     const data = path.includes("/task") ? body.taskData : body;
-    console.log(data);
     for (const key in data) {
       data[key] = data[key].trim();
     }
-    console.log(data);
+    const { info, ...required } = data;
+
+    !Object.values(required).every((value) => value === true) &&
+      console.log("some required fields are not provided");
   }
   next();
 };
