4476bed84fcecbf252f278fa7862f861240efb39
MaximeArn <maxime-arnould@outlook.fr>
2021-02-19 16:46:06 +0100

    trim differently list request and task request

--- server/src/middlewares/trimData.ts
+++ server/src/middlewares/trimData.ts
@@ -1,9 +1,17 @@
 import { Request, Response, NextFunction } from "express";
 
-export default ({ method, body }: Request, res: Response, next: NextFunction) => {
+export default ({ method, body, path }: Request, res: Response, next: NextFunction) => {
   if (method !== "GET" && method !== "DELETE") {
-    for (const key in body) {
-      body[key] = body[key].trim();
+    if (path.includes("/task")) {
+      const { taskData } = body;
+      for (const key in taskData) {
+        taskData[key] = taskData[key].trim();
+      }
+    } else {
+      console.log("list method");
+      for (const key in body) {
+        body[key] = body[key].trim();
+      }
     }
   }
   next();
