4d103694250313fc35d487dba605c5de2a336c6a
MaximeArn <maxime-arnould@outlook.fr>
2021-02-19 16:49:55 +0100

    factorize body trim depending of the request's path

--- server/src/middlewares/trimData.ts
+++ server/src/middlewares/trimData.ts
@@ -2,17 +2,12 @@ import { Request, Response, NextFunction } from "express";
 
 export default ({ method, body, path }: Request, res: Response, next: NextFunction) => {
   if (method !== "GET" && method !== "DELETE") {
-    if (path.includes("/task")) {
-      const { taskData } = body;
-      for (const key in taskData) {
-        taskData[key] = taskData[key].trim();
-      }
-    } else {
-      console.log("list method");
-      for (const key in body) {
-        body[key] = body[key].trim();
-      }
+    const data = path.includes("/task") ? body.taskData : body;
+    console.log(data);
+    for (const key in data) {
+      data[key] = data[key].trim();
     }
+    console.log(data);
   }
   next();
 };
