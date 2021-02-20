8ab52eb712f4ac3058edc18f3f85e4dcd8bfdc37
MaximeArn <maxime-arnould@outlook.fr>
2021-02-20 16:44:22 +0100

    fix fetch lists problem

--- server/src/middlewares/trimData.ts
+++ server/src/middlewares/trimData.ts
@@ -1,15 +1,22 @@
 import { Request, Response, NextFunction } from "express";
 
 export default ({ method, body, path }: Request, res: Response, next: NextFunction) => {
-  if (method !== "GET" && method !== "DELETE") {
-    const data = path.includes("/task") ? body.taskData : body;
-    for (const key in data) {
-      data[key] = data[key].trim();
-    }
-    const { info, ...required } = data;
+  try {
+    if (method !== "GET" && method !== "DELETE") {
+      const data = path.includes("/task") ? body.taskData : body;
+      for (const key in data) {
+        data[key] = data[key].trim();
+      }
+      const { info, ...required } = data;
 
-    !Object.values(required).every((value) => value === true) &&
-      console.log("some required fields are not provided");
+      if (!Object.values(required).every((value) => value === true)) {
+        // throw new Error("some required fields are not provided");
+        console.log("some required fields are not provided");
+      }
+    }
+    next();
+  } catch (error) {
+    // res.send(error);
+    next();
   }
-  next();
 };
